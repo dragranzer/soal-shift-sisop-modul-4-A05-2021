e2015198ad670b1d57fa052f70f5a2605aa598f1
Ryg <ryangarnetandrianto@gmail.com>
2021-06-02 09:43:44 +0700

    Update Sementara_Atbash_Cipher.c

--- Sementara_Atbash_Cipher.c
+++ Sementara_Atbash_Cipher.c
@@ -10,6 +10,7 @@ void encodeAtbash(char *s) {
 }
 
 void decodeAtbash(char *s) {
+    // Decode Atbash Cipher string
     for (int i = 0; s[i]; i++) {
         if ('A' <= s[i] && s[i] <= 'Z') s[i] = 'A'-s[i]+'Z';
         else if ('a' <= s[i] && s[i] <= 'z') s[i] = 'a'-s[i]+'z';
